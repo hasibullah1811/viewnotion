ad9c8faecabb15f2f0743bb1a58948c7cf8bd167
Hasib Ullah <hasib.ascend@gmail.com>
2020-06-05 18:37:14 +0600

    Image Picker from Gallery Added

--- lib/screen/add_wallpaper_screen.dart
+++ lib/screen/add_wallpaper_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddWallpaperScreen extends StatefulWidget {
   @override
@@ -6,12 +9,46 @@ class AddWallpaperScreen extends StatefulWidget {
 }
 
 class _AddWallpaperScreenState extends State<AddWallpaperScreen> {
+  File _image;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Wallpaper'),
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: <Widget>[
+              InkWell(
+                onTap: () {
+                  // Function to upload the image
+                  _loadImage();
+                },
+                child: _image != null
+                    ? Image.file(_image)
+                    : Image(
+                        image: AssetImage(
+                            'assets/images/placeholder-upload-img.png'),
+                      ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Click on the image above to upload your image"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  void _loadImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
   }
 }
